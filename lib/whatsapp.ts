a45152275baf2fb15b1f330e02aec97b4392c980
EnricoPG <pgenrico@gmail.com>
2026-08-14 21:56:49 -0600

    Delete lib directory

--- lib/whatsapp.ts
+++
@@ -1,4 +0,0 @@
-export function buildWhatsAppUrl(number: string, message: string) {
-  const cleanNumber = number.replace(/\D/g, "");
-  return `https://wa.me/${cleanNumber}?text=${encodeURIComponent(message)}`;
-}
